aa5905366ee9375ea4a2cd9ca6f898223bcb2ae6
Samwakelam <sam.wakelam@gymshark.com>
2022-02-25 08:08:17 +0000

    fix: undefined typeerror

--- src/helpers/get-grouped-object.helper.ts
+++ src/helpers/get-grouped-object.helper.ts
@@ -1,8 +1,10 @@
 import { ForecastDataProps } from '../@types/resort-data.definition';
 
-export const getGroupedObject = (arr: any[], key: string) => {
-    return arr.reduce((prev, cur) => {
-        (prev[cur[key]] = prev[cur[key]] || []).push(cur);
-        return prev;
-    }, {});
+export const getGroupedObject = (arr: any[], key: string): { [keyof: string]: ForecastDataProps[] } => {
+    if (arr.length > 0) {
+        return arr.reduce((prev, cur) => {
+            (prev[cur[key]] = prev[cur[key]] || []).push(cur);
+            return prev;
+        }, {});
+    }
 };
--- src/layouts/index/index.layout.tsx
+++ src/layouts/index/index.layout.tsx
@@ -54,7 +54,7 @@ export const IndexLayout = ({ activeLocation, activeLevel, activeWeather, cardDa
 
         const newArray = [];
         for (let i = 0; i < keys.length; i++) {
-            const arr = snowStats[Object.keys(snowStats)[i]];
+            const arr: ForecastDataProps[] = snowStats[Object.keys(snowStats)[i]];
 
             const totalPrecipitation = arr.reduce((prev, curr) => {
                 return prev + curr.precip_mm;
